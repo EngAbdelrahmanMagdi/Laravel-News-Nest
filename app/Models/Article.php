aed0a88a2b13a598d66ba1ea57c806df45e30a8c
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2025-03-14 23:46:58 +0200

    Article Model

---
+++ app/Models/Article.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'summary', 'api_source', 'url',
+        'category', 'published_at', 'image_url', 'author_id', 'category_id', 'source_id',
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
+}
+
